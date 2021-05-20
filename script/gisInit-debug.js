1ff6e952d31fba2b4c71b27d5669bcf928d24377
Dreamice <dreamice13@foxmail.com>
2021-05-20 18:02:46 +0800

    调整引用文件

--- script/gisInit-debug.js
+++ script/gisInit-debug.js
@@ -2,4 +2,7 @@
  * Created by dreamice on 2021/5/118.
  */
 var gis_version = 1.0;
-$LAB.script("script/modules/common.js?version="+gis_version).wait();
+$LAB.script("script/modules/common.js?version="+gis_version)
+.script("script/modules/style.js?version="+gis_version)
+.script("script/modules/locate.js?version="+gis_version).wait()
+.script("script/modules/main.js?version="+gis_version);
