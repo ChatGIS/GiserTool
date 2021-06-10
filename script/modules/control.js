ae4392bb531552b52bcb09d401af842f580263c3
Dreamice <dreamice13@foxmail.com>
2021-06-10 20:22:39 +0800

    初步添加tooltip

--- script/modules/control.js
+++ script/modules/control.js
@@ -1,4 +1,38 @@
 var control = {
+	/**
+	 * The measure tooltip element.
+	 * @type {HTMLElement}
+	 */
+	measureTooltipElement : null,
+	/**
+	 * Overlay to show the measurement.
+	 * @type {Overlay}
+	 */
+	measureTooltip : null,
+	
+	/**
+	 * Creates a new measure tooltip
+	 */
+	createMeasureTooltip : function() {
+	  if (control.measureTooltipElement) {
+	    control.measureTooltipElement.parentNode.removeChild(control.measureTooltipElement);
+	  }
+	  control.measureTooltipElement = document.createElement('div');
+	  control.measureTooltipElement.className = 'ol-tooltip ol-tooltip-measure';
+	  control.measureTooltip = new ol.Overlay({
+	    element: control.measureTooltipElement,
+	    offset: [0, -15],
+	    positioning: 'bottom-center',
+	  });
+	  map.addOverlay(control.measureTooltip);
+	},
+	
+	/* 
+		测量控件相关函数：开始
+	 */
+	/* 
+	  清除地图交互
+	 */
 	clearAllMapInteraction : function(){
 	    /* var obj1 = document.getElementById('map-marking-info');
 	    removeClass(obj1, "show"); */
@@ -13,6 +47,9 @@ var control = {
 	    }
 	},
 	
+	/* 
+		添加测量交互
+	 */
 	addMeasureInteraction : function(type) {
 	    //document.getElementById('map-marking-panel-title').innerHTML = type == 'area' ? '面积测量' : '长度测量';
 	    //openPanelContent(['map-marking-panel', 'measure-info']);
@@ -25,6 +62,7 @@ var control = {
 	    });
 	    map.addInteraction(draw);
 	    // createMeasureTooltip();
+		control.createMeasureTooltip();
 	    // createHelpTooltip();
 	    var listener;
 	    draw.on('drawstart', function(evt) {
@@ -45,18 +83,24 @@ var control = {
 	            }
 	            // measureTooltipElement.innerHTML = output;
 	            // measureTooltip.setPosition(tooltipCoord)
+				control.measureTooltipElement.innerHTML = output;
+				control.measureTooltip.setPosition(tooltipCoord);
 				console.log(output);
 				$("#location-coordinate").val(output);
 				
 	        })
 	    }, this);
 	    draw.on('drawend', function() {
-	        measureTooltipElement.className = 'tooltip tooltip-static';
+	        // measureTooltipElement.className = 'tooltip tooltip-static';
+			control.measureTooltipElement.className = 'ol-tooltip ol-tooltip-static';
 	        measureTooltip.setOffset([0, -7]);
 	        sketch = null;
-	        measureTooltipElement = null;
-	        createMeasureTooltip();
+	        control.measureTooltipElement = null;
+	        control.createMeasureTooltip();
 	        ol.Observable.unByKey(listener)
 	    }, this)
 	},
+	/*
+		测量控件相关函数：结束
+	 */
 }
